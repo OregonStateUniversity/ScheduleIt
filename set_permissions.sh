3c0ee2041ffbb95c537966efd3d251a4190987f0
Bangbay Siboliban <bangbay@gmail.com>
2020-07-03 22:58:29 -0700

    chore(phpunit): add initial tests

--- set_permissions.sh
+++ set_permissions.sh
@@ -10,15 +10,30 @@ chmod 600 composer*
 
 # Executable scripts
 chmod 700 *.sh
+<<<<<<< HEAD
 chmod 700 .git/hooks/pre-commit
+=======
+chmod 700 .git/hooks/pre-push
+>>>>>>> f27e509... chore(phpunit): add initial tests
 
 # Folders with executables
 chmod -R 700 bin
 chmod 755 bin
 chmod -R 700 vendor/bin
 chmod 755 vendor/bin
+<<<<<<< HEAD
 chmod -R 700 vendor/squizlabs/php_codesniffer/bin
 chmod 755 vendor/squizlabs/php_codesniffer/bin
+=======
+chmod 700 vendor/phpunit/phpunit/phpunit
+
+# Folders with executables
+chmod -R 700 bin
+chmod 755 bin
+chmod -R 700 vendor/bin
+chmod 755 vendor/bin
+chmod 700 vendor/phpunit/phpunit/phpunit
+>>>>>>> f27e509... chore(phpunit): add initial tests
 
 # Private folders
 chmod 700 docs
