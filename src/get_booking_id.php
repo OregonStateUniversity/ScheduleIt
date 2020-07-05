d5ae2ab429f09917062db696ca8ad6ae5ca3b3cb
mc-csharpclass <36938152+mc-csharpclass@users.noreply.github.com>
2020-07-06 07:35:43 +0900

    this update allows an event creator to remove a registered attendee
    upadated the info at the top of the file
    
    feat: event creator can removed attendees.
    
    chore(php): run php files through linter
    
    chore(php): comment spacing
    
    feat: event creators can now remove attendees from their events

---
+++ src/get_booking_id.php
@@ -0,0 +1,17 @@
+<?php
+
+// set up session
+require_once dirname(__DIR__) . '/config/session.php';
+
+// set up connection to database via MySQLi
+require_once dirname(__DIR__) . '/config/database.php';
+
+$database->connectAsAdministrator();
+
+// get data from POST request
+$slotId = $_POST["id"];
+$attendeeOnid = $_POST["onid"];
+
+// delete reservation
+$id = $database->creatorDeleteReservation($slotId, $attendeeOnid);
+echo json_encode($id);
