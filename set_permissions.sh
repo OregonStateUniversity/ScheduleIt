7b6afafa6e2a70b0289a8461b17d31e9319ef197
Bangbay Siboliban <bangbay@gmail.com>
2020-07-05 22:11:50 -0700

    fix(phpunit): change version to compatible with php7.2
    - fix commitlint action
    - remove merge conflict artifacts in set_permissions

--- set_permissions.sh
+++ set_permissions.sh
@@ -4,27 +4,23 @@ find . -type f -exec chmod 644 {} \;
 
 # Config files in root
 chmod 755 .htaccess
-chmod 600 .editorconfig
-chmod 600 .gitignore
+# Add -f to files that aren't necessary to be on server
+chmod 600 -f .editorconfig
+chmod 600 -f  .gitignore
 chmod 600 composer*
 
 # Executable scripts
 chmod 700 *.sh
-<<<<<<< HEAD
-chmod 700 .git/hooks/pre-commit
-=======
-chmod 700 .git/hooks/pre-push
->>>>>>> f27e509... chore(phpunit): add initial tests
+chmod 700 -f .git/hooks/pre-commit
+chmod 700 -f .git/hooks/pre-push
 
 # Folders with executables
 chmod -R 700 bin
 chmod 755 bin
 chmod -R 700 vendor/bin
 chmod 755 vendor/bin
-<<<<<<< HEAD
 chmod -R 700 vendor/squizlabs/php_codesniffer/bin
 chmod 755 vendor/squizlabs/php_codesniffer/bin
-=======
 chmod 700 vendor/phpunit/phpunit/phpunit
 
 # Folders with executables
@@ -33,7 +29,6 @@ chmod 755 bin
 chmod -R 700 vendor/bin
 chmod 755 vendor/bin
 chmod 700 vendor/phpunit/phpunit/phpunit
->>>>>>> f27e509... chore(phpunit): add initial tests
 
 # Private folders
-chmod 700 docs
+chmod 700 -f docs
