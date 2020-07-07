fcd9e8dbe15f9a18125eda2fb4feb2156094105a
Bangbay Siboliban <bangbay@gmail.com>
2020-07-07 07:47:50 -0700

    fix(require-dev): remove php-cs-fixer because of incompatibilty (#8)
    * fix(require-dev): remove php-cs-fixer because of incompatibilty
    
    * chore(editorconfig): set indent size for php

--- set_permissions.sh
+++ set_permissions.sh
@@ -5,30 +5,30 @@ find . -type f -exec chmod 644 {} \;
 # Config files in root
 chmod 755 .htaccess
 # Add -f to files that aren't necessary to be on server
-chmod 600 -f .editorconfig
-chmod 600 -f  .gitignore
+chmod -f 600 .editorconfig
+chmod -f 600 .gitignore
 chmod 600 composer*
 
 # Executable scripts
 chmod 700 *.sh
-chmod 700 -f .git/hooks/pre-commit
-chmod 700 -f .git/hooks/pre-push
+chmod -f 700 .git/hooks/pre-commit
+chmod -f 700 .git/hooks/pre-push
 
 # Folders with executables
 chmod -R 700 bin
 chmod 755 bin
-chmod -R 700 vendor/bin
-chmod 755 vendor/bin
-chmod -R 700 vendor/squizlabs/php_codesniffer/bin
-chmod 755 vendor/squizlabs/php_codesniffer/bin
-chmod 700 vendor/phpunit/phpunit/phpunit
+chmod -Rf 700 vendor/bin
+chmod -f 755 vendor/bin
+chmod -Rf 700 vendor/squizlabs/php_codesniffer/bin
+chmod -f 755 vendor/squizlabs/php_codesniffer/bin
+chmod -f 700 vendor/phpunit/phpunit/phpunit
 
 # Folders with executables
 chmod -R 700 bin
 chmod 755 bin
-chmod -R 700 vendor/bin
-chmod 755 vendor/bin
-chmod 700 vendor/phpunit/phpunit/phpunit
+chmod -Rf 700 vendor/bin
+chmod -f 755 vendor/bin
+chmod -f 700 vendor/phpunit/phpunit/phpunit
 
 # Private folders
-chmod 700 -f docs
+chmod -f 700 docs
