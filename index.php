2a4bcca18124def366877d95018c66697b5b3e01
Bangbay Siboliban <bangbay@gmail.com>
2020-07-07 22:53:27 -0700

    feat(layouts): update layout names

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
 
-// set up session
-
-require_once dirname(__FILE__) . '/config/session.php';
-
-// set up connection to database via MySQLi
-
-require_once dirname(__FILE__) . '/config/database.php';
-
 // set up twig
 
 require_once dirname(__FILE__) . '/config/twig.php';
 
 // render page using twig
 
-echo $twig->render('views/main.twig');
+echo $twig->render('views/home.twig');
