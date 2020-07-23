f4ab28d2138daa8e085fa384e670f7fc3d2cc993
mc-csharpclass <36938152+mc-csharpclass@users.noreply.github.com>
2020-07-24 04:01:04 +0900

    fix: removed debug code

--- scheduleit/views/meetings/index.php
+++ scheduleit/views/meetings/index.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 require_once ABSPATH . 'config/session.php';
 
 $search_term = !empty($_GET['q']) ? $_GET['q'] : null;
