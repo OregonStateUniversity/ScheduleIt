5efb48af0db22aad244cb5faad1021c9a067b37f
mc-csharpclass <36938152+mc-csharpclass@users.noreply.github.com>
2020-07-28 22:11:17 +0900

    feat: invite by onid from _meeting_details_mine.twig

--- scheduleit/views/meetings/show.php
+++ scheduleit/views/meetings/show.php
@@ -4,15 +4,45 @@ require_once ABSPATH . 'config/session.php';
 
 $meeting = $database->getMeetingById($meeting_id, $_SESSION['user']);
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $attendeeOnids = $_POST['attendeeOnid'];
+    $link = $_POST['link'];
+    $link = "http://web.engr.oregonstate.edu" . $link;
+    $host = $_SESSION['user'];
+    $hash = $meeting['hash'];
+    // turn onid string into array
+    $onidArray = explode(" ",$attendeeOnids);
+    
+    // create email list
+    // send email in forloop so that other recipients emails are not
+    // exposed
+    foreach ($onidArray as $onid) {
+      if (strlen($onid) > 2) {
+        
+        $email = $onid . '@oregonstate.edu';
+
+        // create message
+        $msgFormat = 
+        "Hi %s, \n\nYour are invited to %s's event! Please follow this address to reserve a seat:\n\n%s";
+
+        $headers = "From: Schedule It" . "\r\n";
+
+        $msg = sprintf($msgFormat, $onid, $host, $link);
+
+        $result = mail($email, "You're Invited", $msg, $headers);
+      }
+    }
+}
+
 if ($meeting) {
     $meeting['dates'] = $database->getDatesByMeetingId($meeting['id']);
     $meeting['dates_count'] = count($meeting['dates']);
     $attendee_meetings = $database->getMeetingAttendees($meeting_id);
-
+    
     echo $twig->render('meetings/show.twig', [
         'attendee_meetings' => $attendee_meetings,
         'meeting' => $meeting,
-        'title' => $meeting['name'],
+        'title' => $meeting['name'],        
     ]);
 } else {
     http_response_code(404);
