fae5769e4343d20fa10a218828c96652f61c5f73
Bangbay Siboliban <bangbay@gmail.com>
2020-07-30 02:20:21 -0700

    feat(cards): group attendees on single card

--- scheduleit/views/meetings/remove_attendee.php
+++ scheduleit/views/meetings/remove_attendee.php
@@ -3,19 +3,15 @@
 require_once ABSPATH . 'config/session.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      
      $removeOnid = $_POST['attendeeOnid'];
      $slotHash = $_POST['slotHash'];
      $removeOnid = trim($removeOnid);
-     $slotHash = trim($slotHash); 
+     $slotHash = trim($slotHash);
      
      $result = $database->deleteBooking($removeOnid, $slotHash);
           
      echo json_encode($result);
-     if ($result > 0) {
-         // add email here
-      
-     }
- }
-
- 
+    if ($result > 0) {
+        // add email here
+    }
+}
--- scheduleit/views/meetings/show.php
+++ scheduleit/views/meetings/show.php
@@ -7,52 +7,50 @@ $meeting = $database->getMeetingById($meeting_id, $_SESSION['user_id']);
 $inviteList = $database->getNotRegistered($meeting['id']);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  if (isset($_POST['attendeeOnid'])) {
-    $attendeeOnids = $_POST['attendeeOnid'];
-    $link = $_POST['link'];
-    $link = $_SERVER['HTTP_ORIGIN'] . $link;
-    $host = $_SESSION['user'];
-    $hash = $meeting['hash'];
-    // turn onid string into array
-    $onidArray = explode(" ",$attendeeOnids);
+    if (isset($_POST['attendeeOnid'])) {
+        $attendeeOnids = $_POST['attendeeOnid'];
+        $link = $_POST['link'];
+        $link = $_SERVER['HTTP_ORIGIN'] . $link;
+        $host = $_SESSION['user'];
+        $hash = $meeting['hash'];
+      // turn onid string into array
+        $onidArray = explode(" ", $attendeeOnids);
 
-    // create email list
-    // send email in forloop so that other recipients emails are not
-    // exposed
-    $sentInvites = 0;
-    foreach ($onidArray as $onid) {
-      if (strlen($onid) > 2) {
+      // create email list
+      // send email in forloop so that other recipients emails are not
+      // exposed
+        $sentInvites = 0;
+        foreach ($onidArray as $onid) {
+            if (strlen($onid) > 2) {
+                $email = $onid . '@oregonstate.edu';
 
-        $email = $onid . '@oregonstate.edu';
+                // create message
+                $msgFormat =
+                "Hi %s, \n\nYour are invited to %s's event! Please follow this address to reserve a seat:\n\n%s";
 
-        // create message
-        $msgFormat =
-        "Hi %s, \n\nYour are invited to %s's event! Please follow this address to reserve a seat:\n\n%s";
+                $headers = "From: Schedule It" . "\r\n";
 
-        $headers = "From: Schedule It" . "\r\n";
+                $message = sprintf($msgFormat, $onid, $host, $link);
 
-        $message = sprintf($msgFormat, $onid, $host, $link);
+                $result = mail($email, "You're Invited", $message, $headers);
 
-        $result = mail($email, "You're Invited", $message, $headers);
-
-        if ($result == 1) {
-          // add onid to the events inivte list
-          $database->insertInviteList($onid, $meeting['id']);
-          $sentInvites += 1;
+                if ($result == 1) {
+                  // add onid to the events inivte list
+                    $database->insertInviteList($onid, $meeting['id']);
+                    $sentInvites += 1;
+                }
+            }
         }
-      }
-    }
-    if ($sentInvites > 1) {
-      $successMessage = 'Sent ' . $sentInvites . ' invites.';
-      $msg->success($successMessage, SITE_DIR . '/meetings/' . $meeting['id']);
+        if ($sentInvites > 1) {
+            $successMessage = 'Sent ' . $sentInvites . ' invites.';
+            $msg->success($successMessage, SITE_DIR . '/meetings/' . $meeting['id']);
+        } else {
+            if ($sentInvites > 0) {
+                $msg->success('Sent 1 invite.', SITE_DIR . '/meetings/' . $meeting['id']);
+            }
+        }
     } else {
-      if ($sentInvites > 0) {
-        $msg->success('Sent 1 invite.', SITE_DIR . '/meetings/' . $meeting['id']);
-      }
     }
-  } else {
-
-  }
 }
 
 if ($meeting && $meeting['creator_id'] == $_SESSION['user_id']) {
