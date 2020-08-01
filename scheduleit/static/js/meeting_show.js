7336885c3ca2b1ecae12763424308c3948bc2ac0
mc-csharpclass <36938152+mc-csharpclass@users.noreply.github.com>
2020-08-02 05:07:13 +0900

    fix: including js file changes

--- scheduleit/static/js/meeting_show.js
+++ scheduleit/static/js/meeting_show.js
@@ -4,21 +4,22 @@ $(document).ready(function () {
   var url = $(this).attr('data-url');
   var onid = $(this).attr('data-attendee-onid');
   var slotHash = $(this).attr('data-timeslot-hash');
+  var meetingHash = $(this).attr('data-meeting-hash');
   $("#delete-attendee-modal").modal("show");
   
     $("#delete-attendee-button").click( function () {
       $("#delete-attendee-modal").modal("hide");
-      removeAttendee(slotHash, onid, meetingName, url);
+      removeAttendee(slotHash, onid, meetingName, meetingHash, url);
     });
   });
 });  
 
 
-function removeAttendee(slotHash, onid, meetingName, url) {
+function removeAttendee(slotHash, onid, meetingName, meetingHash, url) {
   $.ajax({
     url: url,
     type: "POST",
-    data: { slotHash: slotHash, attendeeOnid: onid, eventName: meetingName }
+    data: { slotHash: slotHash, attendeeOnid: onid, meetingName: meetingName, meetingHash: meetingHash }
   }).done(function (response) {
     $("#delete-attendee-modal").modal("hide");
     location.reload();
