1391f6329f7f8005fa0895c9fa16ae187c82b147
Bangbay Siboliban <bangbay@gmail.com>
2020-08-01 01:52:20 -0700

    chore: formatting

--- scheduleit/static/styles.php
+++ scheduleit/static/styles.php
@@ -1,16 +1,19 @@
 <?php
 
-    // This file loads all of the CSS files in the css directory.
-    header('Content-type: text/css');
-    $path_to_css = __DIR__ . '/css';
+// This file loads all of the CSS files in the css directory.
+header('Content-type: text/css');
+$path_to_css = __DIR__ . '/css';
+
 function get_files($dir = '.', $sort = 0)
 {
     $files = scandir($dir, $sort);
     $files = array_diff($files, array('.', '..'));
     return $files;
 }
-    $files = get_files($path_to_css, 1);
-    sort($files);
+
+$files = get_files($path_to_css, 1);
+sort($files);
+
 foreach ($files as $file) {
     include_once($path_to_css . '/' . $file);
 }
